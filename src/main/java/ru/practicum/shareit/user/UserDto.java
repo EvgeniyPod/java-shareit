101b793e378dc93cd8132f5c60742aa17064ae50
Евгений Подольский <evgeniy@podolskiyrsb.ru>
2024-04-09 23:24:06 +0300

    ТЗ-13

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -4,12 +4,11 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
-public class User {
+public class UserDto {
     private Long id;
-    @Email @NotNull private String email;
+    @Email private String email;
     private String name;
 }
