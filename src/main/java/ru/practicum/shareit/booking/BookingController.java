404c563c1f5fe73861e4236cc4e6102bdadba7ec
Евгений Подольский <evgeniy@podolskiyrsb.ru>
2024-05-04 18:11:14 +0300

    fix: добавлена коснтанта

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -3,15 +3,7 @@ package ru.practicum.shareit.booking;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 
 import javax.validation.Valid;
@@ -28,18 +20,19 @@ import java.util.List;
 @Validated
 public class BookingController {
     private final BookingService bookingService;
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
 
     @PostMapping
-    public Booking addBookingRequest(@RequestHeader("X-Sharer-User-Id") long userId,
+    public Booking addBookingRequest(@RequestHeader(USER_ID_HEADER) long userId,
                                      @Valid @RequestBody BookingDto bookingDto) {
         log.info("Request for booking item {} from user {}", bookingDto.getItemId(), userId);
         return bookingService.createBooking(userId, bookingDto);
     }
 
     @PatchMapping("/{bookingId}")
-    public Booking updateItem(@RequestHeader("X-Sharer-User-Id") long userId,
-                           @PathVariable long bookingId,
-                           @RequestParam boolean approved) {
+    public Booking updateItem(@RequestHeader(USER_ID_HEADER) long userId,
+                              @PathVariable long bookingId,
+                              @RequestParam boolean approved) {
         if (approved) {
             log.info("Request for approving booking request {} from user {}",
                     bookingId, userId);
@@ -51,25 +44,25 @@ public class BookingController {
     }
 
     @GetMapping("/{bookingId}")
-    public Booking getBookingById(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable long bookingId) {
+    public Booking getBookingById(@RequestHeader(USER_ID_HEADER) long userId, @PathVariable long bookingId) {
         log.info("Request for get booking request {} from user {}", bookingId, userId);
         return bookingService.getBookingById(userId, bookingId);
     }
 
     @GetMapping
-    public List<Booking> getBookingsByState(@RequestHeader("X-Sharer-User-Id") long userId,
+    public List<Booking> getBookingsByState(@RequestHeader(USER_ID_HEADER) long userId,
                                             @RequestParam(required = false) String state,
                                             @PositiveOrZero @RequestParam(defaultValue = "0", required = false) Integer from,
-                                            @PositiveOrZero @RequestParam(defaultValue = "50", required = false) Integer size)  {
+                                            @PositiveOrZero @RequestParam(defaultValue = "50", required = false) Integer size) {
         log.info("Request for get {} bookings in state {} from user {} from {}", size, state, userId, from);
         return bookingService.getBookingByState(userId, state, from, size);
     }
 
     @GetMapping("/owner")
-    public List<Booking> getBookingsByOwnerAndState(@RequestHeader("X-Sharer-User-Id") long userId,
+    public List<Booking> getBookingsByOwnerAndState(@RequestHeader(USER_ID_HEADER) long userId,
                                                     @RequestParam(required = false) String state,
                                                     @RequestParam(required = false) Integer from,
-                                                    @RequestParam(required = false) Integer size)  {
+                                                    @RequestParam(required = false) Integer size) {
         log.info("Request for get {} bookings of user {} in state {} from {}", size, userId, state, from);
         return bookingService.getBookingsByOwnerAndState(userId, state, from, size);
     }
